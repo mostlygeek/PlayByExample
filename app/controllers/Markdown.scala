0635b40e195b3a006c685e36ed66edb620ae748d
Benson Wong <mostlygeek@gmail.com>
2012-06-15 12:22:23 -0700

    added first cut of markdown source show

--- app/controllers/Markdown.scala
+++ app/controllers/Markdown.scala
@@ -12,15 +12,23 @@ import org.clapper.markwrap._
  *   - feel awesome.
  *
  * Design: 
- *   - all markdown files (*.md) are stored in app/content
- *   - this controller *only* looks in app/content like a chroot
- *   - 
+ *   - all markdown files (*.md) are stored in app/public/markdown/somefile.md
+ *   - this controller *only* looks in that directory for the content
  */
 object Markdown extends Controller {
   //ref: http://software.clapper.org/markwrap/#parsing_markdown 
-  private lazy val mdparser = MarkWrap.parserFor(MarkupType.Markdown)
-  
+  private val mdparser = MarkWrap.parserFor(MarkupType.Markdown)
+ 
+  val base = Play.current.path + "/public/markdown" 
   def load(path: String) = Action {
-    Ok("Requested: " + path)
+    val file = Play.current.getFile("public/markdown/" + path)
+    
+    // make sure the file is under the base, and that it exists...
+    if ((file.getCanonicalPath startsWith base) && file.exists()) {
+      val html = mdparser.parseToHTML(io.Source.fromFile(file))
+      Ok(views.html.markdown(html))
+    }
+    else 
+      NotFound
   }
 }
